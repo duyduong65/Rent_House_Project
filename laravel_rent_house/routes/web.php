e2a93f202f3eac67c5b37851021f5f6f5da52c0b
quyenmanucian98 <ledanhquyen1998@gmail.com>
2019-12-05 15:06:14 +0700

    refactor app.view

--- laravel_rent_house/routes/web.php
+++ laravel_rent_house/routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::prefix('houses')->group(function () {
     Route::get('/', 'HouseController@index')->name('houses.index');
