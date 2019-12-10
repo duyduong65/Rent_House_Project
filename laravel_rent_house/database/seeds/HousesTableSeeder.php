a067f871040ec43fd0d1deefd3a6298fc395c15f
quyenmanucian98 <ledanhquyen1998@gmail.com>
2019-12-10 15:31:24 +0700

    refactor footer

--- laravel_rent_house/database/seeds/HousesTableSeeder.php
+++ laravel_rent_house/database/seeds/HousesTableSeeder.php
@@ -24,5 +24,45 @@ class HousesTableSeeder extends Seeder
         $house->price = 230.000;
         $house->city_id = 2;
         $house->save();
-        }
+
+        $house = new House();
+        $house->name = 'Vietcombank Tower';
+        $house->image = 'img/rooms/vietcombank.jpg';
+        $house->kindHouse = 'Home Stay';
+        $house->kindRoom = 'Phòng đôi';
+        $house->address = 'Hà Nội';
+        $house->numBedroom = '2';
+        $house->numBathroom = '2';
+        $house->description = 'Tọa lạc tại ngay mặt đường Pham Hùng, cửa ngõ phía Tây Hà Nội. Keangnam có tổng mức đầu tư cho toàn bộ công trình là 1,05 tỷ USD, bao gồm 2 tòa chung cư cao cấp 48 tầng và tòa nhà tổ hợp văn phòng, khách sạn, trung tâm giải trí 72 tầng.';
+        $house->price = 230.000;
+        $house->city_id = 2;
+        $house->save();
+
+        $house = new House();
+        $house->name = 'FLC Twin Tower A';
+        $house->image = 'img/rooms/FLC.jpg';
+        $house->kindHouse = 'Home Stay';
+        $house->kindRoom = 'Phòng đôi';
+        $house->address = 'Tọa lạc tại địa chỉ 265 Cầu Giấy';
+        $house->numBedroom = '2';
+        $house->numBathroom = '2';
+        $house->description = 'Trên nền diện tích hơn 1 ha sẽ được xây dựng 1 tòa tháp đôi, trong đó tháp căn hộ có chiều cao 50 tầng và tháp văn phòng là 38 tầng, với 3 tầng hầm để xe. 5 tầng đế dành cho chức năng thương mại với diện tích sàn khu thương mại là 25.000 m2; khu căn hộ có tổng diện tích 66.484 m2 và khu văn phòng có tổng diện tích sàn 35.960 m2. Với mật độ xây dựng khối chân đế là 48,6%, dự án sẽ tạo ra một không gian sinh hoạt chung nhiều sắc xanh, thoáng đạt cho toàn bộ cư dân.';
+        $house->price = 230.000;
+        $house->city_id = 2;
+        $house->save();
+
+        $house = new House();
+        $house->name = 'Lotte Center';
+        $house->image = 'img/rooms/lotte.jpg';
+        $house->kindHouse = 'Home Stay';
+        $house->kindRoom = 'Phòng đôi';
+        $house->address = 'Hà Nội';
+        $house->numBedroom = '2';
+        $house->numBathroom = '2';
+        $house->description = 'Từ tầng 1 đến tầng 6 là trung tâm thương mại với các mặt hàng thời trang cao cấp. Trên đó là khu văn phòng cho thuê hạng A và căn hộ sang trọng, khách sạn 5 sao. Tại tầng nóc của tòa nhà Lotte được bố trí quán bar và Đài quan sát Sky Walk giúp bạn có thể quan sát gần như toàn bộ thành phố Hà Nội.';
+        $house->price = 230.000;
+        $house->city_id = 2;
+        $house->save();
+    }
+
 }
--- laravel_rent_house/resources/views/layouts/app.blade.php
+++ laravel_rent_house/resources/views/layouts/app.blade.php
@@ -429,8 +429,7 @@
             <nav class="main-menu mobile-menu">
                 <ul>
                     <li class="active"><a href="{{route('home')}}">Home</a></li>
-                    <li><a href="./about-us.html">Houses</a></li>
-                    <li><a href="{{route('houses.list')}}">Rooms</a></li>
+                    <li><a href="{{route('houses.list')}}">Houses</a></li>
                     <li><a href="#">Pages</a>
                         <ul class="drop-menu">
                             <li><a href="about-us.html">About Us</a></li>
@@ -439,8 +438,7 @@
                         </ul>
                     </li>
                     <li><a href="./blog.html">News</a></li>
-                    <li><a href="{{route('houses.create')}}">Upload House <i class="fa fa-upload"
-                                                                             style="font-size:30px;color:red"></i></a>
+                    <li><a href="{{route('houses.create')}}">Upload House</a>
                     </li>
                 </ul>
             </nav>
@@ -455,64 +453,13 @@
 <footer class="footer-section">
     <div class="container">
         <div class="row">
-            <div class="col-lg-4">
-                <div class="footer-item">
-                    <div class="footer-logo">
-                        <a href="#"><img src="{{asset('storage/img/logo.png')}}" alt=""></a>
-                    </div>
-                    <p>Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                        dolore magna aliqua.</p>
+            <div class="col-lg-12 ">
+                <div class="small text-white text-center mb-3">
+                    &copy;<script>document.write(new Date().getFullYear());</script>
+                    Bản quyền thuộc về Công ty TNHH VTM Việt Nam. Mọi hành vi
+                    sao chép đều là phạm pháp nếu không có sự cho phép bằng văn bản của chúng tôi
                 </div>
             </div>
-            <div class="col-lg-4">
-                <div class="footer-item">
-                    <h5>Newsletter</h5>
-                    <div class="newslatter-form">
-                        <input type="text" placeholder="Your Email Here">
-                        <button type="submit">Subscribe</button>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="footer-item">
-                    <h5>Contact Info</h5>
-                    <ul>
-                        <li><img src="{{asset('storage/img/placeholder.png')}}" alt="">1525 Boring Lane,<br/>Los
-                            Angeles, CA
-                        </li>
-                        <li><img src="{{asset('storage/img/phone.png')}}" alt="">+1 (603)535-4592</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="copyright">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-
-                    <ul>
-                        <li class="active"><a href="./index.html">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Rooms</a></li>
-                        <li><a href="#">Facilities</a></li>
-                        <li><a href="#">News</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="row pt-5">
-                <div class="col-lg-12 ">
-                    <div class="small text-white text-center">
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        Copyright &copy;<script>document.write(new Date().getFullYear());</script>
-                        All rights reserved | This template is made with <i class="fa fa-heart-o"
-                                                                            aria-hidden="true"></i> by <a
-                            href="https://colorlib.com" target="_blank">Colorlib</a>
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
-                </div>
-            </div>
-
         </div>
     </div>
 </footer>
